26423a0b969253e796b5d4e692f0b36be7cf1c4c
saidhhgomez <saidhuallancagomez@gmail.com>
2025-11-04 13:43:12 -0500

    config general y dependencias, conexion bd

---
+++ rest-restaurant-api/src/main/java/com/restaurant/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.restaurant.config;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class AppConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+        return resources;
+    }
+}
