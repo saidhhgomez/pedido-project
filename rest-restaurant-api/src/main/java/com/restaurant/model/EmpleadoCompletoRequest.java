83f6cb7d8edd06723751c6fd168bcff27d5541b9
saidhhgomez <saidhuallancagomez@gmail.com>
2025-11-04 15:17:35 -0500

    registro completo

--- rest-restaurant-api/src/main/java/com/restaurant/model/EmpleadoCompletoRequest.java
+++ rest-restaurant-api/src/main/java/com/restaurant/model/EmpleadoCompletoRequest.java
@@ -4,22 +4,27 @@ public class EmpleadoCompletoRequest {
     private Credenciales credenciales;
     private Persona persona;
     private Empleado empleado;
-	public Credenciales getCredenciales() {
-		return credenciales;
-	}
-	public void setCredenciales(Credenciales credenciales) {
-		this.credenciales = credenciales;
-	}
-	public Persona getPersona() {
-		return persona;
-	}
-	public void setPersona(Persona persona) {
-		this.persona = persona;
-	}
-	public Empleado getEmpleado() {
-		return empleado;
-	}
-	public void setEmpleado(Empleado empleado) {
-		this.empleado = empleado;
-	}
+    public Credenciales getCredenciales() {
+        return credenciales;
+    }
+
+    public void setCredenciales(Credenciales credenciales) {
+        this.credenciales = credenciales;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
 }
