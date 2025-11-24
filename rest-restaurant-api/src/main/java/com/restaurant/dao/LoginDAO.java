4c74a449f68ffdad847be749bb571f5dd54305ea
saidhhgomez <saidhuallancagomez@gmail.com>
2025-11-24 00:56:15 -0500

    logica del login

---
+++ rest-restaurant-api/src/main/java/com/restaurant/dao/LoginDAO.java
@@ -0,0 +1,70 @@
+package com.restaurant.dao;
+
+import com.restaurant.config.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashMap;
+
+public class LoginDAO {
+
+    public HashMap<String, Object> login(String usuario) {
+    	
+        HashMap<String, Object> data = new HashMap<>();
+    	
+    	String sql = "SELECT " +
+    	        "  cr.idCredencial, cr.usuario, cr.contrasena, " +
+    	        "  p.idPersona, p.nombres, p.apPaterno, p.apMaterno, " +
+    	        "  c.idCliente, " +
+    	        "  e.idEmpleado, r.nombre AS rolNombre " +
+    	        "FROM Credenciales cr " +
+    	        "JOIN Persona p ON p.idCredencial = cr.idCredencial " +
+    	        "LEFT JOIN Cliente c ON c.idPersona = p.idPersona " +
+    	        "LEFT JOIN Empleado e ON e.idPersona = p.idPersona " +
+    	        "LEFT JOIN Contrato con ON con.idEmpleado = e.idEmpleado AND con.estadoContrato = 'activo' " +
+    	        "LEFT JOIN Roles r ON r.idRol = con.idRol " +
+    	        "WHERE cr.usuario = ?";
+
+        try (Connection cn = DBConnection.getConnection();
+             PreparedStatement ps = cn.prepareStatement(sql)) {
+
+            ps.setString(1, usuario);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                data.put("contrasena", rs.getString("contrasena"));
+
+                String nombre = rs.getString("nombres") + " " +
+                                rs.getString("apPaterno") + " " +
+                                rs.getString("apMaterno");
+                data.put("nombre", nombre.trim());
+                
+                int idCliente = rs.getInt("idCliente");
+                int idEmpleado = rs.getInt("idEmpleado");
+
+                if (idCliente > 0) {
+                    data.put("tipoId", "cliente");
+                    data.put("id", idCliente);
+                    data.put("rol", "cliente"); // DEFECTO
+                }
+
+                if (idEmpleado > 0) {
+                    data.put("tipoId", "empleado");
+                    data.put("id", idEmpleado);
+                    
+                    // SI TIENE ROL
+                    String rolNombre = rs.getString("rolNombre");
+                    data.put("rol", rolNombre != null ? rolNombre.toLowerCase() : "empleado");
+                }
+
+                return data;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error en login DAO: " + e.getMessage());
+        }
+
+        return null; // USER NO EXISTE
+    }
+}
