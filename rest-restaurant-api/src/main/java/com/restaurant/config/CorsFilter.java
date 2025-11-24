42e24f037194039e3d0d1ce60a508e01406d5d98
Johan Davis <116416429+JohanDB09@users.noreply.github.com>
2025-11-24 00:38:29 -0500

    funcinalidad-registrar-cliente

--- rest-restaurant-api/src/main/java/com/restaurant/config/CorsFilter.java
+++ rest-restaurant-api/src/main/java/com/restaurant/config/CorsFilter.java
@@ -1,5 +1,33 @@
 package com.restaurant.config;
 
-public class CorsFilter {
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
 
+@Provider
+@PreMatching
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        // Si es OPTIONS, devolver 200 OK inmediatamente
+        if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
+            requestContext.abortWith(Response.ok().build());
+        }
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
 }
