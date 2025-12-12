bcc501e1f69cc8f0169de675385927673f3e1583
saidhhgomez <saidhuallancagomez@gmail.com>
2025-12-12 13:43:55 -0500

    agregar resources a la raiz

--- rest-restaurant-api/src/main/java/com/restaurant/config/AppConfig.java
+++ rest-restaurant-api/src/main/java/com/restaurant/config/AppConfig.java
@@ -7,8 +7,14 @@ import javax.ws.rs.core.Application;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 import com.restaurant.resource.EmpleadoResource;
+import com.restaurant.resource.CatalogoComidaResource;
 import com.restaurant.resource.ClienteResource;
-import com.restaurant.resource.FileResource;
+import com.restaurant.resource.DireccionClienteResource;
+import com.restaurant.resource.LoginResource;
+import com.restaurant.resource.MetodoPagoResource;
+import com.restaurant.resource.RolResource;
+import com.restaurant.resource.SucursalResource;
+import com.restaurant.resource.TipoContratoResource;
 
 @ApplicationPath("/api")
 public class AppConfig extends Application {
@@ -16,8 +22,14 @@ public class AppConfig extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new HashSet<>();
         resources.add(EmpleadoResource.class); 
+        resources.add(CatalogoComidaResource.class); 
+        resources.add(LoginResource.class); 
+        resources.add(DireccionClienteResource.class); 
+        resources.add(MetodoPagoResource.class); 
+        resources.add(RolResource.class); 
+        resources.add(SucursalResource.class); 
+        resources.add(TipoContratoResource.class); 
         resources.add(ClienteResource.class); 
-        resources.add(FileResource.class);
         resources.add(MultiPartFeature.class); // Muy importante
         return resources;
     }
